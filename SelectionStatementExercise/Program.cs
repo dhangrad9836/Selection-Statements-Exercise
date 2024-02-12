8bf57bb8d2ea4dd63babaee000a708a57c439a93
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-02-12 09:03:14 -0500

    Completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Initialize an integer to represent a favorite number and assign a value with
+            //a number of your choosing or you can assign a random number to this variable:
+
+            //create a random number
+            var r = new Random();
+
+            //generate a random number between 1 and 1000
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine($"The random num is: {favNumber}");
+            //ask user toguess your favorite number is
+            Console.WriteLine("Can you guess my favorite number? ");
+
+            //get user input and store in userInput variable
+            var userInput = int.Parse(Console.ReadLine());
+
+            //validate if user input is too low, too high, or if it's a correct guess
+            if(userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guesst it!!!");
+            }
+
+
+            Console.WriteLine(favNumber);
+
+            
+
+            Console.ReadLine();
+
         }
     }
 }
